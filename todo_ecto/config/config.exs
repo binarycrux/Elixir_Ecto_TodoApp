14cae66dabc181d09ffa03af24495e9cd6b65a53
binarycrux <147232142+binarycrux@users.noreply.github.com>
2025-02-12 16:47:27 +0300

    Update config.exs

--- todo_ecto/config/config.exs
+++ todo_ecto/config/config.exs
@@ -2,8 +2,8 @@ import Config
 
 config :todo_ecto, TodoEcto.Repo,
   database: "todos",
-  username: "postgres",
-  password: "Post@localhost127!",
+  username: "your-username",
+  password: "your-password",
   hostname: "localhost"
 
 config :todo_ecto, ecto_repos: [TodoEcto.Repo]
